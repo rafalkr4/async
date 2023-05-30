4c93a6e5fbbd8d51c7893bf13519be85f9c56b2b
Rafał Krasowski <rrafal.ka@gmail.com>
2023-05-30 11:07:33 +0200

    promise using kebab example, remember kebab joke krakow, kazimierz, 3 w nocy, gastro

--- app.js
+++ app.js
@@ -1,26 +1,46 @@
-const boxRef = document.querySelector('.box');
+// const boxRef = document.querySelector('.box');
+//
+//
+// let isFinished = false;
+//
+// // callback hell
+// setTimeout(() => {
+//     boxRef.style.left = '500px';
+//     setTimeout(() => {
+//         boxRef.style.top = '500px';
+//         setTimeout(() => {
+//             boxRef.style.left = '0px';
+//             setTimeout(() => {
+//                 boxRef.style.top = '0px';
+//                 isFinished = true;
+//             }, 3000);
+//         }, 3000);
+//     }, 3000);
+// }, 3000)
+//
+//
+// setTimeout(() => {
+//     if (isFinished) {
+//         console.log('Run again');
+//     }
+// }, 15000)
 
 
-let isFinished = false;
-
-// callback hell
-setTimeout(() => {
-    boxRef.style.left = '500px';
+const receipt = new Promise((resolve, reject) => {
     setTimeout(() => {
-        boxRef.style.top = '500px';
-        setTimeout(() => {
-            boxRef.style.left = '0px';
-            setTimeout(() => {
-                boxRef.style.top = '0px';
-                isFinished = true;
-            }, 3000);
-        }, 3000);
+        resolve("Kebab maj friend!");
+    }, 3001);
+
+    setTimeout(() => {
+        reject("No kebab, łonna fajt")
     }, 3000);
-}, 3000)
+});
 
-
-setTimeout(() => {
-    if (isFinished) {
-        console.log('Run again');
-    }
-}, 15000)
+console.log(receipt)
+receipt
+    .then((msg) => {
+        console.log(msg)
+    })
+    .catch((msg) => {
+        console.log(msg)
+    });
